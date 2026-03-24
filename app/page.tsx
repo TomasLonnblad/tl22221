48b70706a46410022b3d32e90eb94d9ef2355490
TomasLonnblad <tomaslonnblad@gmail.com>
2026-03-24 08:39:49 +0100

    x

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Component from '@/components/sparkles-fullpage-demo'
+
+export default function Page() {
+  return (
+    <main className="flex min-h-svh items-center justify-center">
+      <Component />
+    </main>
+  )
+}
